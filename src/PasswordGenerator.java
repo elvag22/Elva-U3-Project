9bad3d433807ad4b52b9d5f1f3e12391a7719b46
elvag22 <elva.gao@thekaustschool.org>
2025-12-16 22:37:39 +0300

    Push 1: Imported all needed libraries

---
+++ src/PasswordGenerator.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.*;
+import org.jfree.chart.*;
+import org.jfree.chart.plot.*;
+import org.jfree.data.category.*;
+
+public class PasswordGenerator {
+}
