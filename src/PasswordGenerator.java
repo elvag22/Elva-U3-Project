ef11d48f411f964e4f9f5d2e38123b8348c3b623
elvag22 <elva.gao@thekaustschool.org>
2025-12-17 01:26:17 +0300

    Update PasswordGenerator.java

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -9,4 +9,23 @@ import org.jfree.chart.plot.*;
 import org.jfree.data.category.*;
 
 public class PasswordGenerator {
+    private JFrame frame;
+    private JTextField passwordChoices;
+    private JButton generateButton, chartButton;
+    private ArrayList<String> passwords = new ArrayList<>();
+    private ArrayList<Integer> lengths = new ArrayList<>();
+    private Random rand = new Random();
+
+    public PasswordGenerator() {
+        createGUI()
+    }
+
+    private void createGUI() {
+        frame = new JFrame("Password Generator!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 200);
+        frame.setLayout(new FlowLayout());
+
+        JLabel passwordLabel = new JLabel("Your Password:");
+        frame.add(passwordLabel);
 }
