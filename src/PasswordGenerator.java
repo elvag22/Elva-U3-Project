e09c6f7e43c07dbed318be18bb82691db5c6e3bf
elvag22 <elva.gao@thekaustschool.org>
2025-12-18 18:11:10 +0300

    Update PasswordGenerator.java

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -17,31 +17,34 @@ public class PasswordGenerator {
     private ArrayList<Integer> lengths = new ArrayList<>();
     private Random rand = new Random();
 
+    // sets GUI to make it visible
     public PasswordGenerator() {
         createGUI();
         frame.setVisible(true);
     }
 
+    //Creates maoin GUI
     private void createGUI() {
         frame = new JFrame("Password Generator!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 200);
         frame.setLayout(new FlowLayout());
-
+//label for password display when generated
         JLabel passwordLabel = new JLabel("Your Password:");
         frame.add(passwordLabel);
-
+//non user-ediatble field for password display
         passwordChoices = new JTextField(20);
         passwordChoices.setEditable(false);
         frame.add(passwordChoices);
-
+//button to generate password
         generateButton = new JButton("Click to make password!");
         frame.add(generateButton);
         generateButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                passwordLengthChoice();
+                passwordLengthChoice();//ask user for password length
             }
         });
+        //button to display charts of password lengths
         chartButton = new JButton("Show password lengths");
         frame.add(chartButton);
         chartButton.addActionListener(new ActionListener() {
@@ -53,7 +56,7 @@ public class PasswordGenerator {
 
     }
 
-
+    //asks user for desired password length with conditionals to catch errors
     private void passwordLengthChoice() {
         String input = JOptionPane.showInputDialog(frame, "Enter how long you want your password to be as an integer");
         if (input == null) return;
@@ -67,6 +70,7 @@ public class PasswordGenerator {
             passwordChoices.setText(password);
             passwords.add(password);
             lengths.add(length);
+            //saves password in csv
             try {
                 savePassword(password, length);
             } catch (IOException ex) {
@@ -78,6 +82,7 @@ public class PasswordGenerator {
         }
     }
 
+    //pool of chars to choose from
     private String generatePassword(int length) {
         String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         String password = "";
@@ -88,7 +93,8 @@ public class PasswordGenerator {
 
     }
 
-    //START CHATGPT CITATION
+    //This function was written by ChatGPT (OpenAI GPT-5)
+    // Accessed on: 2025-12-16
     private void savePassword(String password, int length) throws IOException {
         File file = new File("passwords.csv");
         boolean newFile = file.createNewFile();
@@ -100,9 +106,10 @@ public class PasswordGenerator {
             writer.write(password + "," + length);
             writer.newLine();
         }
-        //END CHATGPT CITATION
+        //ChatGPT entry ends hyere
     }
 
+    //creates a bar chart of every password generated so far
     private void showChart() {
         if (lengths.isEmpty()) {
             JOptionPane.showMessageDialog(frame, "No passwords are made yet!");
@@ -111,17 +118,18 @@ public class PasswordGenerator {
 
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
         for (int i = 0; i < passwords.size(); i++) {
+            //add data for chart
             dataset.addValue(lengths.get(i), "Passoword Length", "Password Number" + (i + 1));
         }
-
         JFreeChart chart = ChartFactory.createBarChart(
-                "Password Lengths,",
-                "Password",
-                "Length",
+                "Password Lengths,", // title
+                "Password", // x axis
+                "Length", // y axis
                 dataset,
                 PlotOrientation.VERTICAL,
                 false, true, false
         );
+//display chart in a new frame
 
         ChartFrame chartFrame = new ChartFrame("Password Length Chart", chart);
         chartFrame.setSize(500, 400);
